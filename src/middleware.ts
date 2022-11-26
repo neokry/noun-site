4ce45a76386ecc7f5428573f87477632717edae9
neokry <patrickgenevich@gmail.com>
2022-11-26 13:44:14 -0800

    Add caching

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,8 +10,6 @@ export default function middleware(req: NextRequest) {
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const hostname = req.headers.get("host") || "demo.vercel.pub";
 
-  console.log("hostname", hostname);
-
   // Only for demo purposes - remove this if you want to use your root domain as the landing page
   if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
     return NextResponse.redirect("https://demo.vercel.pub");
@@ -19,7 +17,7 @@ export default function middleware(req: NextRequest) {
 
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname.replace(`.vercel.pub`, "")
+      ? hostname.replace(`.builer-site.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
   // rewrites for app pages
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -41,4 +41,17 @@ export const trpc = createTRPCNext<AppRouter>({
    * @link https://trpc.io/docs/ssr
    **/
   ssr: true,
+  responseMeta({ clientErrors, ctx }) {
+    if (clientErrors.length) {
+      // propagate first http error from API calls
+      return {
+        status: clientErrors[0].data?.httpStatus ?? 500,
+      };
+    }
+    // cache full page for 1 day + revalidate once every minute
+    const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+    return {
+      "Cache-Control": `s-maxage=60, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+    };
+  },
 });
