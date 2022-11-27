f667039221f0f0f32b5b480d77e1a160f934a0c0
neokry <patrickgenevich@gmail.com>
2022-11-26 21:46:49 -0800

    Test cache

--- src/pages/api/token/[address]/index.ts
+++ src/pages/api/token/[address]/index.ts
@@ -27,16 +27,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     tokenContract.auction(),
   ]);
 
-  const contractInfo = parseBase64String(contractURI);
+  const contractJSON = parseBase64String(contractURI);
 
-  return {
-    ...contractInfo,
-    image: getNormalizedURI(contractInfo.image, {
+  const contractInfo = {
+    ...contractJSON,
+    image: getNormalizedURI(contractJSON.image, {
       preferredIPFSGateway: process.env.NEXT_PUBLIC_IPFS_GATEWAY,
     }),
     total_supply: total_supply.toHexString(),
     auction,
   } as ContractInfo;
+
+  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=59");
+  res.send(contractInfo);
 };
 
 export default handler;
