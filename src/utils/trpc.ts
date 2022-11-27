4fe15a7b1180a4a8760cdefc853fb5c3b6c6cd44
neokry <patrickgenevich@gmail.com>
2022-11-26 21:17:55 -0800

    change client requests

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,12 +1,8 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
-import type { AppRouter } from "@/server/routers/_app";
+import type { AppRouter } from "../server/routers/_app";
 
 function getBaseUrl() {
-  if (typeof window !== "undefined")
-    // browser should use relative path
-    return "";
-
   if (process.env.VERCEL_URL)
     // reference for vercel.com
     return `https://${process.env.VERCEL_URL}`;
