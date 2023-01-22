9937654d3cb4431f7e4c97f96ffc97e49260681e
Volky <volkyeth@proton.me>
2023-01-22 14:45:42 -0300

    Fix invisible settle button text on dark mode

--- components/Hero/SettleAuction.tsx
+++ components/Hero/SettleAuction.tsx
@@ -23,7 +23,7 @@ export const SettleAuction = ({ auction }: { auction?: string }) => {
   return (
     <button
       onClick={() => write?.()}
-      className="w-full bg-skin-button-accent hover:bg-skin-button-accent-hover h-12 text-white mt-6 rounded-lg flex items-center justify-around"
+      className="w-full bg-skin-button-accent bg-skin-button-accent hover:bg-skin-button-accent-hover text-skin-inverted h-12 mt-6 rounded-lg flex items-center justify-around"
     >
       {isLoading ? (
         <Image src="/spinner.svg" height={26} width={26} alt="spinner" />
